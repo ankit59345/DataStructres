da6ab84c9685ab72a69632cae1148fdf551e0735
ankit59345 <ankit.kumar59345@gmail.com>
2020-01-20 20:37:06 +0530

    To reverse words in a sentence seperated by '.'

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ReverseString {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number of Test Cases");
+		int tCase = in.nextInt();
+		while(tCase > 0) {
+			System.out.println("Enter the String to reverse");
+			tCase--;
+		}
+	}
+}
